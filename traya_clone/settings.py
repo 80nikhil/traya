1a87b70f84747c19832b301ae1aca0eebdbf0e64
Nikhil Karole <94338426+80nikhil@users.noreply.github.com>
2024-05-17 03:05:19 +0530

    Update settings.py

--- traya_clone/settings.py
+++ traya_clone/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(&w(jn$3+rco%!rre=*s0d)z0wm+fg4^w$r-def)%ij+8%d9#2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
