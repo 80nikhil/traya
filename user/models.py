c938eea3dcccf450b0ec939b87296f707997256f
80nikhil <nikhilkarole808@gmail.com>
2024-07-15 15:07:02 +0530

    changes in database

--- user/models.py
+++ user/models.py
@@ -18,4 +18,8 @@ class order(models.Model):
     user = models.ForeignKey(user,on_delete=models.CASCADE)
     status = models.CharField(max_length=15, choices=Order_Status,default='ORDERED')
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+class transaction_history(models.Model):    
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)    
